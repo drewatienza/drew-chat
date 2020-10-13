6ed35732c76eb1726478ec8d750a01f8d8b4e614
drewatienza <atienza.drew@gmail.com>
2020-10-13 13:11:37 -0700

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyCe0DloNR_oak29e8Qk4WAwkpmYEKmbk1I",
+  authDomain: "chat-web-app-58ab8.firebaseapp.com",
+  databaseURL: "https://chat-web-app-58ab8.firebaseio.com",
+  projectId: "chat-web-app-58ab8",
+  storageBucket: "chat-web-app-58ab8.appspot.com",
+  messagingSenderId: "988512455135",
+  appId: "1:988512455135:web:eb69e584ae86314beddc6a"
+};
+
+const app = firebase.initializeApp(config);
+
