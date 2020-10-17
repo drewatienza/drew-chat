d7af3a603e1596f6604518e3d69cadd9a30a26ce
drewatienza <atienza.drew@gmail.com>
2020-10-17 11:35:26 -0700

    created global profile state management with context

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,11 +1,55 @@
-import React, { createContext, useContext, useState } from 'react';
+import { database } from 'firebase';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../misc/firebase';
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profile] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return <ProfileContext.Provider value={profile}>
+  useEffect(() => {
+
+    let userRef;
+    
+    const authUnsub = auth.onAuthStateChanged( authObj => {
+      if(authObj) {
+
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        
+        userRef.on('value', (snapshot) => {
+          const {name, createAt} = snapshot.val();
+
+          const data = {
+            name,
+            createAt,
+            uid: authObj.uid,
+            email: authObj.email
+          }
+          setProfile(data);
+          setIsLoading(false);
+        })
+        
+      } else {
+        if(userRef) {
+          userRef.off()
+        }
+
+        setProfile(null);
+        setIsLoading(false);
+      }
+    } );
+
+    return () => {
+      authUnsub();
+
+      if(userRef) {
+        userRef.off();
+      }
+    }
+  }, [])
+
+  return <ProfileContext.Provider value={{ isLoading, profile }}>
     {children}
   </ProfileContext.Provider>
 }
